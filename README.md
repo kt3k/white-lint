ec4884e983b0e473968de8dfbc877633513626ed
Yoshiya Hinosawa <stibium121@gmail.com>
2015-11-22 18:07:38 +0900

    Initial prototype

---
+++ README.md
@@ -0,0 +1,46 @@
+# white-lint v1.0.0
+
+> A tool for creating a cli tool like [standard](https://github.com/feross/standard) lint checker.
+
+# Install
+
+```
+npm install --save white-lint
+```
+
+# Usage
+
+```js
+var myLint = require('white-lint')({
+    cmdName: 'snowball',
+    version: '1.0.0'
+    cmdOptions: {
+        eslintOptions: {
+            "options": "..."
+        },
+        ignore: [],
+        globals: [],
+        parser: "parser"
+    }
+})
+
+// execute lint as a node API
+myLint.lint()
+
+// execute lint as a cli
+myLint.lintAsCli()
+```
+
+# Options
+
+## cmdName: String
+
+The name of the command.
+
+## version: String
+
+The version name
+
+## cmdOptions: Object
+
+TBD
---
+++ index.js
@@ -0,0 +1,55 @@
+var subclass = require('subclassjs')
+
+var Application = require('./lib/Application')
+
+/**
+ * The white lint module interface.
+ *
+ * @class
+ */
+var WhiteLint = subclass(function (pt) {
+
+    /**
+     * @param {Object} options The options
+     * @param {String} options.cmdName The command name
+     * @param {String} options.version The version name
+     * @param {String} options.cmdOptions The command default options
+     */
+    pt.constructor = function (options) {
+
+        if (!(this instanceof WhiteLint)) {
+
+            return new WhiteLint(options)
+
+        }
+
+        this.brutus = require('brutus')({
+            name: options.cmdName,
+            version: options.version,
+            options: options.cmdOptions,
+            delegate: new Application(options)
+        })
+
+    }
+
+    /**
+     * Lints as an API.
+     */
+    pt.lint = function () {
+
+        this.brutus.execute()
+
+    }
+
+    /**
+     * Lints as an CLI.
+     */
+    pt.lintAsClit = function () {
+
+        this.brutus.executeAsCli()
+
+    }
+
+})
+
+module.exports = WhiteLint
